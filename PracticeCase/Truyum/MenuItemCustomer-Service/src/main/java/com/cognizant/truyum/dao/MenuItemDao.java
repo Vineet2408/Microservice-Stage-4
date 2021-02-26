a8a5c76851e3c72cffb0b322057ce104198f6807
Vineet2408 <vineetd84@gmail.com>
2021-02-26 18:01:28 +0530

    practice case and final case

---
+++ PracticeCase/Truyum/MenuItemCustomer-Service/src/main/java/com/cognizant/truyum/dao/MenuItemDao.java
@@ -0,0 +1,12 @@
+package com.cognizant.truyum.dao;
+
+import java.util.List;
+
+import com.cognizant.truyum.model.MenuItem;
+
+public interface MenuItemDao 
+{
+	public List<MenuItem> getMenuItemListCustomer();
+	public MenuItem getMenuItem(long menuItemId);
+
+}
---
+++ PracticeCase/Truyum/MenuItemCustomer-Service/src/main/java/com/cognizant/truyum/dao/MenuItemDaoCollectionImpl.java
@@ -0,0 +1,67 @@
+package com.cognizant.truyum.dao;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+import com.cognizant.truyum.model.MenuItem;
+import com.cognizant.truyum.util.DateUtil;
+@Component("menuItemDao")
+public class MenuItemDaoCollectionImpl implements MenuItemDao{
+	
+	private List<MenuItem> menuItemList;
+	
+
+	public MenuItemDaoCollectionImpl() {
+		super();
+		ApplicationContext context=new ClassPathXmlApplicationContext("truyum.xml");
+		List<MenuItem> list=(List<MenuItem>) context.getBean("menuItemList");
+		this.menuItemList=list;
+		
+	}
+
+	
+	
+	@Override
+	public List<MenuItem> getMenuItemListCustomer()
+	{
+		// TODO Auto-generated method stub
+		List<MenuItem> menuItemListCustomer=new ArrayList<>();
+		Date date =new Date();
+		SimpleDateFormat simpleDateFormat=new SimpleDateFormat("dd/MM/yyyy");
+		String ds=simpleDateFormat.format(date);
+		Date ds1=DateUtil.convertToDate(ds);
+		
+		List<MenuItem> list=this.menuItemList;
+		for(MenuItem m:list)
+		{
+			Date d1=m.getDateOfLaunch();
+			if((d1.before(ds1)||d1.equals(ds1))&& m.isActive())
+			{
+				menuItemListCustomer.add(m);
+			}
+		}
+		return menuItemListCustomer;
+	}
+	
+	@Override
+	public MenuItem getMenuItem(long menuItemId) 
+	{
+		// TODO Auto-generated method stub
+		List<MenuItem> list=this.menuItemList;
+		for(MenuItem m:list)
+		{
+			if(m.getId()==menuItemId)
+			{
+				return m;
+			}
+		}
+		return null;
+		
+	}
+
+	
+}
